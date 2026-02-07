88afd036611cabc9a652696a93c64766f9fca39c
MrPrasaD69 <prasadwaghmare6@gmail.com>
2026-02-07 21:07:36 +0530

    laravel echo added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 import axios from 'axios';
 window.axios = axios;
 
@@ -10,3 +12,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import './echo';
+
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    forceTLS: false,
+    enabledTransports: ['ws', 'wss'],
+});
